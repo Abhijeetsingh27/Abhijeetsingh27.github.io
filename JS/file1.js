84885269f3d1937761f49d3842777751c6a4e825
Abhijeetsingh27 <abhijeetsingh271103@gmail.com>
2025-06-20 10:01:22 +0530

    update

---
+++ JS/file1.js
@@ -0,0 +1,23 @@
+// console.log("hello")
+// let a=10
+// let b=90
+// console.log(a+b)
+let age =22
+let score                      
+let city =null
+// console.log(age)
+// console.log(typeof(age))
+//console.log(typeof(score))                               UNDEFINED
+//console.log(typeof(city))                                OBJECT
+// let strAge=String(age)                                  TYPE CASTING
+// console.log(typeof(strAge))
+
+// let rating = "5"
+// let strRating = Number(rating)
+// console.log(typeof (strRating))
+
+// let flag ="true"
+// let bflag=Boolean(flag)
+// console.log(typeof(bflag))
+
+
---
+++ JS/file2.js
@@ -0,0 +1,25 @@
+// let a=6
+// let b=2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// let c="10"
+// let d=10
+// console.log(c+d)
+// console.log(c-d)
+// console.log(c*d)
+// let a=6
+// let b=2
+// console.log(a==b)
+
+// let c="4"
+// let d=4
+// console.log(c===d)
+// console.log(c!==d)
+// console.log(c>d)
+// console.log(c<d)
+// console.log(c!==d)
+
